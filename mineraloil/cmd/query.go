22c64c531db2add199b1243afcf123f21ffd3192
Aneesh Abbineni <aabbineni@all.health>
2026-02-15 22:23:48 -0500

    Added minoil query for required fields

--- mineraloil/cmd/query.go
+++ mineraloil/cmd/query.go
@@ -13,21 +13,53 @@ const BERVENV = "./__bervenv__"
 
 var SYSPACKS = path.Join(BERVENV, "syspacks")
 
+type Dependency struct {
+	Name     string
+	Versions []string
+}
+
+type PackageMetadata struct {
+	Version            string       `json:"version"`
+	BerylliumStandards []string     `json:"beryllium_standards"`
+	Dependencies       []Dependency `json:"dependencies"`
+}
+
 // queryCmd represents the query command
 var queryCmd = &cobra.Command{
 	Use:   "query",
 	Short: "Queries a package/packages",
 	Args:  cobra.MinimumNArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
-		fmt.Println("Query called")
 		for _, pack := range args {
 			jsonPath := path.Join(SYSPACKS, pack, "pkg.json")
-			m := map[string]any{}
 			buffer, err := os.ReadFile(jsonPath)
 			if err != nil {
-				return err
+				return fmt.Errorf("Failed to read %s: %w", jsonPath, err)
 			}
-			json.Unmarshal(buffer, &m)
+
+			var pkg PackageMetadata
+			if err := json.Unmarshal(buffer, &pkg); err != nil {
+				return fmt.Errorf("Invalid json in %s: %w", pack, err)
+			}
+
+			fmt.Printf("Package %s\n", pack)
+			fmt.Printf("Version: %s\n", pkg.Version)
+			fmt.Println("Beryllium Standards: ")
+			for i := 0; i < len(pkg.BerylliumStandards); i += 2 {
+				if i+1 < len(pkg.BerylliumStandards) {
+					fmt.Printf("    %s-%s\n", pkg.BerylliumStandards[i], pkg.BerylliumStandards[i+1])
+				}
+			}
+
+			fmt.Println("Dependencies: ")
+			for _, dep := range pkg.Dependencies {
+				fmt.Printf("    %s:\n", dep.Name)
+				for i := 0; i < len(dep.Versions); i += 2 {
+					if i+1 < len(dep.Versions) {
+						fmt.Printf("        %s-%s\n", dep.Versions[i], dep.Versions[i+1])
+					}
+				}
+			}
 		}
 		return nil
 	},
